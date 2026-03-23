0b6c2e0ff9aab6c3d56b3ec3ea4f52cd4f493e8b
Julian Millen <julimillen@gmail.com>
2026-03-23 17:59:24 -0300

    Primer commit ejercicio integrador

---
+++ blogOnline/src/main/java/com/jm/blogOnline/BlogOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.jm.blogOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogOnlineApplication.class, args);
+	}
+
+}
